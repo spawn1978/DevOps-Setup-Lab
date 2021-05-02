ffcfc103e5737ed89ded1209c059f6840d1d0206
spawn1978 <leguizamoncarlos@gmail.com>
2021-05-02 17:31:37 -0300

    Update deploy_devops_apps.sh

--- deploy_devops_apps.sh
+++ deploy_devops_apps.sh
@@ -2,29 +2,35 @@
 # Script for installing apps for DevOps lab
 
 ## Docker CE
-yum install -y yum-utils
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io
-systemctl enable docker
-systemctl start docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
 
 ## Portainer CE
-docker volume create portainer-data
-docker run --detach \
+sudo docker volume create portainer-data
+sudo docker run --detach \
   --publish 8000:8000 --publish 9000:9000 \
   --name=portainer \
   --restart=always \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume portainer-data:/data \
   portainer/portainer-ce
-firewall-cmd --add-port 9000/tcp --permanent
-firewall-cmd --reload
+sudo ufw allow 9000/tcp
+sudo systemctl restart ufw
 
 ## Gitlab CE
 export GITLAB_HOME=/srv
-docker run --detach \
+sudo docker run --detach \
   --publish 443:443 --publish 80:80 \
   --name gitlab \
   --restart always \
@@ -32,35 +38,39 @@ docker run --detach \
   --volume $GITLAB_HOME/gitlab/logs:/var/log/gitlab:Z \
   --volume $GITLAB_HOME/gitlab/data:/var/opt/gitlab:Z \
   gitlab/gitlab-ce:latest
-firewall-cmd --add-port 80/tcp --permanent
-firewall-cmd --add-port 443/tcp --permanent
-firewall-cmd --reload
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo systemctl restart ufw
+
 
 ## Jenkins
-docker run --detach \
+sudo docker run --detach \
   --publish 8080:8080 --publish 50000:50000 \
   --name jenkins \
   --restart always \
   --volume jenkins_home:/var/jenkins_home \
   jenkins/jenkins:lts
-firewall-cmd --add-port 8080/tcp --permanent
-firewall-cmd --reload
+sudo ufw allow 8080/tcp
+sudo systemctl restart ufw
+
 
 ## Sonarqube
-docker run --detach \
-  --publish 9000:9000 \
+sudo docker run --detach \
+  --publish 7000:7000 \
   --name sonarqube \
   --restart always \
-  sonarqube/sonarqube:lts
-firewall-cmd --add-port 9000/tcp --permanent
-firewall-cmd --reload
+  library/sonarqube:lts
+sudo ufw allow 7000/tcp
+sudo systemctl restart ufw
+
 
 ## Selenium
-docker run --detach \
+sudo docker run --detach \
   --publish 4444:4444 \
   --volume /dev/shm:/dev/shm \
   --name selenium \
 selenium/standalone-chrome:3.141.59-yttrium
-firewall-cmd --add-port 4444/tcp --permanent
-firewall-cmd --reload
+sudo ufw allow 4444/tcp
+sudo systemctl restart ufw
+
 
